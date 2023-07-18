57b1b3bd3770150fda0b61a6e484dd7a6d19243e
Bowie <hed.consultor@hotmail.com>
2023-07-18 10:23:43 -0300

    feat: Config  para database.

--- frontend/js.js
+++ frontend/js.js
@@ -4,7 +4,7 @@ $("#btn_salvar").on("click", function() {
     var txt_mensagem = $("#mensagem").val(); 
 
     $.ajax({
-        url: "http://35.224.5.250:30005/",
+        url: "http://10.100.24.223:30006/",
         type: "post",
         data: {nome: txt_nome, mensagem: txt_mensagem},
         beforeSend: function() {
